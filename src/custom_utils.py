f68693a3e8d2887135abf6c0e28554994a7b3b3f
Jennifer Ma <53239177+jma199@users.noreply.github.com>
2022-10-20 21:13:38 -0400

    update custom_utils.py

--- src/custom_utils.py
+++ src/custom_utils.py
@@ -17,7 +17,7 @@ def plot_learning_curve(classifier, X, y, cv=None, scoring=None, figsize=None):
     ax.grid()
     ax.plot(train_sizes, train_scores_mean, "o-", color="r", label="Training score")
     ax.fill_between(train_sizes, train_scores_mean - train_scores_std, train_scores_mean + train_scores_std, alpha = 0.1, color = 'r')
-    ax.plot(train_sizes, test_scores_mean, "o-", color="g", label="Test Score")
+    ax.plot(train_sizes, test_scores_mean, "o-", color="g", label="CV Score")
     ax.fill_between(train_sizes, test_scores_mean - test_scores_std, test_scores_mean + test_scores_std, alpha = 0.1, color = 'g')
     
     ax.set_xlabel("Train size")
@@ -28,26 +28,31 @@ def plot_learning_curve(classifier, X, y, cv=None, scoring=None, figsize=None):
     plt.show()
     
 
-def plot_validation_curve(estimator, X, y, scoring=None, param_range=None, fig_size=None):
+def plot_validation_curve(estimator, X, y, param_name = None, param_range=None, scoring= None, fig_size=None, semilog=False, **kwargs):
     """Plot validation curve.
     Validation curve is used to determine the train and test scores for varying parameter values"""   
-    train_scores, test_scores = validation_curve(estimator, X, y, scoring=scoring, param_range = param_range)
+    train_scores, test_scores = validation_curve(estimator, X, y, param_name = param_name, param_range = param_range, scoring=scoring, **kwargs)
     
     train_scores_mean = np.mean(train_scores, axis=1)
     train_scores_std = np.std(train_scores, axis=1)
     test_scores_mean = np.mean(test_scores, axis=1)
     test_scores_std = np.std(test_scores, axis=1)
 
-    fig, ax = plt.subplots(fig_size=fig_size)
-    ax.plot(param_range, train_scores_mean, 'o-', label='Train', color='b')
-    ax.fill_between(param_range, train_scores_mean - train_scores_std, train_scores_mean + train_scores_std, alpha=0.2, color='b')
-    ax.plot(param_range, test_scores_mean, 'o-', label='Test', color='darkorange')
-    ax.fill_between(param_range, test_scores_mean - test_scores_std, test_scores_mean + test_scores_std, alpha=0.2, color='darkorange')
+    _, ax = plt.subplots(figsize=fig_size)
+
+    if semilog:
+        ax.semilogx(param_range, train_scores_mean, 'o-', label='Train', color='b')
+        ax.semilogx(param_range, test_scores_mean, 'o-', label='Cross Validation', color='darkorange')
+    else:
+        ax.plot(param_range, train_scores_mean, 'o-', label='Train', color='b')
+        ax.plot(param_range, test_scores_mean, 'o-', label='Cross Validation', color='darkorange')
     
-    ax.set_xlabel("param_name")
-    ax.set_ylable("scoring")
+    ax.fill_between(param_range, train_scores_mean - train_scores_std, train_scores_mean + train_scores_std, alpha=0.2, color='b')
+    ax.fill_between(param_range, test_scores_mean - test_scores_std, test_scores_mean + test_scores_std, alpha=0.2, color='darkorange')
+        
+    ax.set_xlabel(param_name)
+    ax.set_ylabel("scoring")
 
     plt.legend(loc="best")
     plt.title("Validation Curve")
     plt.show()
-
